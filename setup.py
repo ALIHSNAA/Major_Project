201798177c9bbb803521b29474d76fdd8f7b7134
Mahirah Khan <abrarkhan86@gmail.com>
2024-04-05 01:07:42 +0530

    folder added

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="A small package for Book Recommender System",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url="https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     author_email="abrarkhan86@gmail.com",
     packages=[SRC_REPO],
     python_requires=">=3.7",
